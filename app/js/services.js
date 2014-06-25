204a38ada14a793ff304ef0e7655e65dd8b09021
Saul <s.shanabrook@gmail.com>
2014-06-24 20:15:28 -0400

    change site to

--- app/js/services.js
+++ app/js/services.js
@@ -14,7 +14,7 @@ myAppServices.factory('Data', ['$resource', '$http',
       office: "Mayor",
       cityWideOffices: ['Mayor', 'Council Chairman', 'Council At-Large'],
       districtWideOffices: ['Council Ward1', 'Council Ward2'],
-      api_url: '//rawgit.com/techforelissa/finance-data/master'
+      api_url: '//rawgit.com/codefordc/finance/master/data'
     };
 
     // function setProperty(obj, prop, get) {
