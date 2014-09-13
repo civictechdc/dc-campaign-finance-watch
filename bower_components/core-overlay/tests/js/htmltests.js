396dd55f410135c4cc0452c3d70d0f853965a0cd
donaldbraman <donald.braman@gmail.com>
2014-09-13 16:27:00 -0400

    removed console logging

---
+++ bower_components/core-overlay/tests/js/htmltests.js
@@ -0,0 +1,5 @@
+htmlSuite('core-overlay', function() {
+  htmlTest('html/core-overlay-basic.html');
+  htmlTest('html/core-overlay-positioning.html');
+  htmlTest('html/core-overlay-positioning-margin.html');
+});
