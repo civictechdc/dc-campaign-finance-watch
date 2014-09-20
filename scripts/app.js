3c9e669a9894d09181101bf9e4341847145c172e
Donald Braman <donald.braman@gmail.com>
2014-09-20 10:19:41 -0400

    remove submenus

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
-document.addEventListener('polymer-ready', function() {
-  var navicon = document.getElementById('navicon');
-  var drawerPanel = document.getElementById('drawerPanel');
-  navicon.addEventListener('click', function() {
-    drawerPanel.togglePanel();
-  });
-});
+// document.addEventListener('polymer-ready', function() {
+//   var navicon = document.getElementById('navicon');
+//   var drawerPanel = document.getElementById('drawerPanel');
+//   navicon.addEventListener('click', function() {
+//     drawerPanel.togglePanel();
+//   });
+// });
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
