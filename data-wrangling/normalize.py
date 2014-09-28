c29f7766f759d4f0a9a27572286fd867f018605d
donaldbraman <donald.braman@gmail.com>
2014-09-28 13:01:20 -0500

    added start of normalizer

---
+++ data-wrangling/normalize.py
@@ -0,0 +1,18 @@
+'''
+This script will normalize the names and addresses.
+It's likely to be a bit complex because there are many, many crazy errors.
+Such is life with publc data.
+
+There are several ways to go about this.  One is with a mapping API. E.g.:
+
+http://brizzled.clapper.org/blog/2012/02/14/simple-address-standardization/
+
+http://gis.stackexchange.com/questions/22339/open-source-address-correction-parser-with-fuzzy-matching
+
+http://stackoverflow.com/questions/4835318/normalize-data-according-to-business-entity-legal-name-class-of-business-dns
+
+http://opendata.stackexchange.com/questions/115/are-there-any-good-libraries-available-for-doing-normalization-of-company-names
+
+
+'''
+
