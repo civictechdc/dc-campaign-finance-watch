68dd89af66ead6cfbf0916a19acf4cf737a6646f
donaldbraman <donald.braman@gmail.com>
2014-10-29 03:41:57 -0400

    updated ocf data

--- data-wrangling/data-ranking.py
+++ data-wrangling/data-ranking.py
@@ -33,33 +33,39 @@ df['donors'] = 1
 df['complete data'] = 0
 df['complete data'] = (pd.notnull(df['Employer Name']) &
                        pd.notnull(df['Employer Address']) |
+                       df['Employer Address'].str.contains('''N/A''') |
                        df['Employer Address'].str.contains('none') |
                        df['Employer Address'].str.contains('unemployed') |
                        df['Employer Address'].str.contains('self') |
                        df['Employer Address'].str.contains('Self') |
+                       df['Employer Name'].str.contains('''N/A''') |
                        df['Employer Name'].str.contains('none') |
                        df['Employer Name'].str.contains('unemployed') |
                        df['Employer Name'].str.contains('self') |
                        df['Employer Name'].str.contains('Self'))
 
-df['some data'] = (pd.notnull(df['Employer Name']) |
+df['required data'] = (pd.notnull(df['Employer Name']) |
+                       df['Employer Name'].str.contains('''N/A''') |
                        df['Employer Name'].str.contains('none') |
                        df['Employer Name'].str.contains('unemployed') |
                        df['Employer Name'].str.contains('self') |
                        df['Employer Name'].str.contains('Self'))
 
 
-subset = df[['Election Year', 'Office', 'Candidate Name', 'complete data', 'some data', 'donors']]
-grouped = subset.groupby(['Election Year', 'Office', 'Candidate Name']).agg({'complete data' : np.mean, 'some data' : np.mean, 'donors' : np.sum }).reset_index()
+subset = df[['Election Year', 'Office', 'Candidate Name', 'complete data', 'required data', 'donors']]
+grouped = subset.groupby(['Election Year', 'Office', 'Candidate Name']).agg({'complete data' : np.mean, 'required data' : np.mean, 'donors' : np.sum }).reset_index()
+
 sorted = grouped.sort('complete data', ascending=False)
 sc = sorted[(sorted['Election Year'] > 2013) & (sorted['donors'] > 25)].reset_index()
 sc['percent data complete'] = sc['complete data'] * 100
-sc['pad'] = np.where(sc['percent data complete']<9.5, ' ', '')
+sc['padc'] = np.where(sc['percent data complete']<9.5, ' ', '')
 sc['percent data complete'] = sc['percent data complete'].round(0).map(int).map(str) + '%'
-sc['percent data complete'] = sc['pad'] + sc['percent data complete']
+sc['percent data complete'] = sc['padc'] + sc['percent data complete']
 
-sc['percent some data'] = sc['some data'] * 100
-sc['percent some data'] = sc['percent some data'].round(0).map(int).map(str) + '%'
+sc['percent data required'] = sc['required data'] * 100
+sc['padr'] = np.where(sc['percent data required']<9.5, ' ', '')
+sc['percent data required'] = sc['percent data required'].round(0).map(int).map(str) + '%'
+sc['percent data required'] = sc['padr'] + sc['percent data required']
 
 sc['candidate'] = sc['Candidate Name'].map(str) + ' ' + sc['Office'].map(str) + ' (' + sc['Election Year'].map(str) + ')'
 sc['rank'] =  list(range(1,len(sc)+1))
@@ -68,10 +74,13 @@ output_file_name = 'data-ranking-this-year.json'
 sc[['rank',
     'candidate',
     'donors',
+    'percent data required',
     'percent data complete'
     ]].to_json(os.path.join(output_dir, output_file_name), orient='split')
 
 sc[['rank',
     'candidate',
     'donors',
+    'percent data required',
     'percent data complete']]
+
