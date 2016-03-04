5ddd2d861fde992a03ecec230dab8863cc1f6113
Michael Kalish <kalishmichael@gmail.com>
2016-03-04 07:59:08 -0500

    Big update to the data Added an unprocessed model Added raw record to each contribution model
    Former-commit-id: 215a1b27c2d130b0d2d68c0ac31515782e6a8cd8

--- models/candidate.js
+++ models/candidate.js
@@ -1,50 +1,32 @@
 var path = require('path'),
-  mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
 
-var config = require('../config/environment');
+var config = require('./../config/environment');
 
 var candidateSchema = new Schema({
-  name: {type:
-    {
-      first: String,
-      middle: String,
-      last: String,
+    name: String,
+    candidateId: {
+        type: String,
+        unique: true,
+        required: true,
+        index: true
     },
-    unique: true,
-    required: true
-  },
-  campaigns: [
-    {
-    position: String,
-    committee: String,
-    year: Number,
-    individualDonorCount: Number,
-    corporateDonorCount: Number
-    }
-  ],
-  positions: [
-    {
-      position: String,
-      period: {
-        _id: false,
-        from: Number,
-        to: Number
-      },
-      individualDonorCount: Number,
-      corporateDonorCount: Number
-    }
-  ]
+    campaigns: [{
+        raceId: String,
+        campaignId: String,
+        raceType: String,
+        raceTypeDetail: String
+    }]
 });
 
-candidateSchema.index({"$**": 'text'});
-
-candidateSchema.virtual('resource').get(function(){
-  return path.join(config.baseUrl, 'candidate', this._id.toString());
+candidateSchema.index({
+    "$**": 'text'
 });
 
-candidateSchema.virtual('displayName').get(function(){
-  return this.name.first + ' ' + this.name.last;
-});
+candidateSchema.virtual('resource')
+    .get(function () {
+        return path.join(config.baseUrl, 'candidate', this._id.toString());
+    });
 
 module.exports = mongoose.model('Candidate', candidateSchema);
