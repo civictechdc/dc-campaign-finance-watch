eceaa3f5baca30c13007c21182db12f516588657
Romoy Headley <romoy@webs.com>
2016-03-05 23:20:56 -0500

    issue-91 adjusting default search

--- client/js/dateRange.jsx
+++ client/js/dateRange.jsx
@@ -31,6 +31,8 @@ class DateRangeComponent extends React.Component {
         this.setState({
             endDate: modifiers.indexOf("selected") > -1 ? null : moment(day.toISOString())
         });
+        console.log(this.state.endDate);
+        console.log(this.state.startDate);
         this.props.onRangeInput(this.state.endDate, this.state.startDate);
     }
 
@@ -50,24 +52,15 @@ class DateRangeComponent extends React.Component {
     }
 
     handleTenYearsPreset(){
-        this.handleYearsPreset(10)
+        this.handleYearsPreset(10);
     }
 
     render(){
-        var start = this.state.startDate.format('YYYY-MM-DD');
-        var end = this.state.endDate.format('YYYY-MM-DD');
-        var label = start + ' - ' + end;
-        if (start === end) {
-            label = start;
-        }
-
         return (
-
             <Grid className="date-range">
                 <h4 className="instructions">
                     3. Select a range of dates to pull data from
                 </h4>
-                <hr/>
                 <ButtonGroup>
                     <Button onClick={this.handleYearPreset.bind(this)}> Last Year </Button>
                     <Button onClick={this.handleTwoYearsPreset.bind(this)}> Last 2 Years </Button>
@@ -75,7 +68,7 @@ class DateRangeComponent extends React.Component {
                 </ButtonGroup>
                 <hr/>
                 <Col className="show-grid" xs={6} md={4}>
-                    <h5> Start Date </h5>
+                    <h5> Custom Start Date </h5>
                     <DayPicker
                         initialMonth={ this.state.startDate.toDate()}
                         onDayClick={this.handleStartDateClick.bind(this)}
@@ -85,7 +78,7 @@ class DateRangeComponent extends React.Component {
                     />
                 </Col>
                 <Col className="show-grid" xs={6} md={4}>
-                    <h5> End Date </h5>
+                    <h5>Custom End Date </h5>
                     <DayPicker
                         initialMonth={this.state.endDate.toDate()}
                         onDayClick={this.handleEndDateClick.bind(this)}
