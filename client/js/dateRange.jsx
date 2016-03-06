bab35849fc058a9a108eb4d6fd8755956549c609
Romoy Headley <romoy@webs.com>
2016-03-05 21:22:22 -0500

    issue-91 start and end date loading graph

--- client/js/dateRange.jsx
+++ client/js/dateRange.jsx
@@ -2,51 +2,66 @@
 
 import moment from 'moment';
 import React from 'react/addons';
-import DateRangePicker from 'react-bootstrap-daterangepicker';
+import DayPicker, { DateUtils } from "react-day-picker";
 import {Button, Glyphicon} from 'react-bootstrap';
 
 class DateRangeComponent extends React.Component {
-    constructor (props) {
+    constructor(props){
         super(props);
         this.minimumDate = moment('01/01/2007', 'MM/DD/YYYY');
         this.maximumDate = moment(new Date());
-        this.state =  {
-			ranges: {
-				'Last Year': [moment().subtract(1, 'years'), moment()],
-				'Last Two Years': [moment().subtract(2, 'years'), moment()]
-			},
-			startDate: moment().subtract(4, 'years'),
-			endDate: moment()
-		};
+        this.state = {
+            ranges: {
+                'Last Year': [moment().subtract(1, 'years'), moment()],
+                'Last Two Years': [moment().subtract(2, 'years'), moment()]
+            },
+            startDate: moment().subtract(4, 'years'),
+            endDate: moment()
+        };
     }
 
-    handleChange (event, picker) {
-        this.setState({'startDate': picker.startDate, 'endDate': picker.endDate});
-        this.props.onRangeInput(picker.endDate, picker.startDate);
+    handleStartDateClick(event, day, modifiers){
+        console.log(day.toISOString());
+        this.setState({
+            startDate: modifiers.indexOf("selected") > -1 ? null : moment(day.toISOString())
+        });
+        this.props.onRangeInput(this.state.endDate, this.state.startDate);
     }
 
-    render () {
+    handleEndDateClick(event, day, modifiers){
+        this.setState({
+            endDate: modifiers.indexOf("selected") > -1 ? null : moment(day.toISOString())
+        });
+    }
+
+    render(){
         var start = this.state.startDate.format('YYYY-MM-DD');
-		var end = this.state.endDate.format('YYYY-MM-DD');
-		var label = start + ' - ' + end;
-		if (start === end) {
-			label = start;
-		}
-        let dateStyle = {width: '50%'};
+        var end = this.state.endDate.format('YYYY-MM-DD');
+
+        console.log(start);
+        console.log(end);
+
+        var label = start + ' - ' + end;
+        if (start === end) {
+            label = start;
+        }
+
         return (
-            <div className="block">
-                <h4 className="instructions">3. Select a range of dates to pull data from.</h4>
-                <DateRangePicker style={dateStyle} startDate={this.state.startDate} endDate={this.state.endDate} ranges={this.state.ranges} onApply={this.handleChange.bind(this)}>
-    						<Button className="selected-date-range-btn" style={{width:'100%'}}>
-    							<div className="pull-left"><Glyphicon glyph="calendar" /></div>
-    							<div className="pull-right">
-    								<span>
-    									{label}
-    								</span>
-    								<span className="caret"></span>
-    							</div>
-    						</Button>
-    				</DateRangePicker>
+
+            <div className="date-range">
+                <h4 className="instructions">
+                    3. Select a range of dates to pull data from
+                </h4>
+                <h3> Start Date </h3>
+                <DayPicker
+                    initialMonth={ moment().subtract(4, 'years').toDate()}
+                    onDayClick={this.handleStartDateClick.bind(this)}
+                />
+                <h3> End Date </h3>
+                <DayPicker
+                    initialMonth={moment().toDate()}
+                    onDayClick={this.handleEndDateClick.bind(this)}
+                />
             </div>
         );
     }
