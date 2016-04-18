7692eaa9cfd3032acdb1ad2cbf3c95d6d5f44bf0
Romoy Headley <romoy@webs.com>
2016-04-18 18:55:06 -0400

    issue-124 adding empty profile picture state

---
+++ importer/addProfilePictureUrlToCandidate.js
@@ -0,0 +1,34 @@
+var Promise = require('bluebird');
+var _ = require('lodash');
+var async = require('async');
+var Candidates = require('../models/candidate');
+
+//Mongoose
+var mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost:27017/dc-campaign-finance', {
+    server: {
+        socketOptions: {
+            keepAlive: 5,
+            connectTimeoutMS: 30000
+        }
+    }
+});
+
+Candidates.find()
+    .then(function (candidates) {
+        candidates.forEach(function (candidate) {
+            Candidates.findByIdAndUpdate(candidate.id, {
+                $set: {
+                    profilePictureUrl: ""
+                }
+            }, function (err, newC) {
+                console.trace(err);
+                console.log(newC);
+            });
+        });
+    })
+    .then(function () {
+        console.log('processed all records');
+        //mongoose.disconnect();
+    });
