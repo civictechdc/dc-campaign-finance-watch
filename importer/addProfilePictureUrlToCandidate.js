789aea1457f4f46305d6d46ae410df9593b5c7f8
Romoy Headley <romoy@webs.com>
2016-05-07 17:07:10 -0400

    issue-124 add importer README

--- importer/addProfilePictureUrlToCandidate.js
+++ importer/addProfilePictureUrlToCandidate.js
@@ -16,19 +16,20 @@ mongoose.connect('mongodb://localhost:27017/dc-campaign-finance', {
 });
 
 Candidates.find()
-    .then(function (candidates) {
-        candidates.forEach(function (candidate) {
+    .then(function(candidates){
+        candidates.forEach(function(candidate){
             Candidates.findByIdAndUpdate(candidate.id, {
                 $set: {
                     profilePictureUrl: ""
                 }
-            }, function (err, newC) {
+            }, function(err, newC){
                 console.trace(err);
                 console.log(newC);
             });
         });
     })
-    .then(function () {
+    .then(function(){
         console.log('processed all records');
-        //mongoose.disconnect();
+        mongoose.disconnect();
+        process.exit();
     });
