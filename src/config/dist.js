1936bb13d3b3bf33ca321f6b5a49af03249cc2ca
Michael Kalish <kalishmichael@gmail.com>
2016-06-27 22:31:39 -0400

    Fixes for logo and background image

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,7 +4,7 @@ import baseConfig from './base';
 
 let config = {
   // appEnv: 'dist',  // feel free to remove the appEnv property here
-  // api: 'production'
+  api: 'production'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
