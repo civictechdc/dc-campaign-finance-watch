7d4e5e65aa7346524fe2870093739e38a6a2e84a
Romoy Headley <romoy@webs.com>
2016-12-23 10:52:20 -0500

    add host to createClient redis is available but mongo is not

--- config/environment/development.js
+++ config/environment/development.js
@@ -4,5 +4,5 @@ module.exports = {
   mongo: {
     uri: 'mongodb://localhost:27017/dc-campaign-finance'
   },
-  baseUrl: 'localhost:8001/dc-campaign-finance/api'
+  baseUrl: '0.0.0.0:8001/dc-campaign-finance/api'
 };
