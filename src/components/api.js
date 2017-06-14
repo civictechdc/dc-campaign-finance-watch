bd2af71c4b3a644aad8b7b41f6283f56bb63c214
Ting Lew <ting.lew@gmail.com>
2017-06-14 13:04:38 -0400

    add try/catch blocks to api

--- src/components/api.js
+++ src/components/api.js
@@ -10,24 +10,32 @@ class Client {
   }
 
   async getCampaignData(campaignId) {
-    const res = await fetch(this.baseUrl + '/contributions/' + campaignId)
-    const json = await res.json()
-    return json
+    try {
+      const res = await fetch(this.baseUrl + '/contributions/' + campaignId)
+      const json = await res.json()
+      return json
+    } catch (err) {
+      console.log('fetch failed', err);
+    }
   }
 
   async getCandidates(toDate, fromDate) {
     const toDateString = toDate.format(dateFormat);
     const fromDateString = fromDate.format(dateFormat);
-    const res = await fetch(
-      this.baseUrl +
-        '/candidate' +
-        '?toDate=' +
-        toDateString +
-        '&fromDate=' +
-        fromDateString
-    )
-    const json = await res.json()
-    return json
+    try {
+      const res = await fetch(
+        this.baseUrl +
+          '/candidate' +
+          '?toDate=' +
+          toDateString +
+          '&fromDate=' +
+          fromDateString
+      )
+      const json = await res.json()
+      return json
+    } catch (err) {
+      console.log('fetch failed', err);
+    }
   }
 
   async getCandidate(candidate) {
@@ -37,46 +45,65 @@ class Client {
       }),
       '&'
     );
-    const res = await fetch(
-      this.baseUrl + '/candidate/' + candidate.id + '?' + campaignSearch
-    )
-    const json = await res.json()
-    return json
+    try {
+      const res = await fetch(
+        this.baseUrl + '/candidate/' + candidate.id + '?' + campaignSearch
+      )
+      const json = await res.json()
+      return json
+    } catch (err) {
+      console.log('fetch failed', err);
+    }
+
   }
 
   async getCampaigns(race, dateRange) {
-    if (Object.keys(dateRange).length === 0) {
+    try {
+      if (Object.keys(dateRange).length === 0) {
+        const res = await fetch(
+          this.baseUrl + '/electionSearch' + '?raceType=' + race
+        )
+        const json = await res.json();
+        return json;
+      }
+
       const res = await fetch(
-        this.baseUrl + '/electionSearch' + '?raceType=' + race
+        this.baseUrl +
+          '/electionSearch' +
+          '?raceType=' +
+          race +
+          '&fromDate=' +
+          dateRange.fromDate.format(dateFormat) +
+          '&toDate=' +
+          dateRange.toDate.format(dateFormat)
       )
       const json = await res.json();
       return json;
+    } catch (err) {
+      console.log('fetch failed', err);
     }
-    const res = await fetch(
-      this.baseUrl +
-        '/electionSearch' +
-        '?raceType=' +
-        race +
-        '&fromDate=' +
-        dateRange.fromDate.format(dateFormat) +
-        '&toDate=' +
-        dateRange.toDate.format(dateFormat)
-    )
-    const json = await res.json();
-    return json;
+
 
   }
 
   async getRaces() {
-    const res = await fetch(this.baseUrl + '/races')
-    const json = await res.json();
-    return json;
+    try {
+      const res = await fetch(this.baseUrl + '/races')
+      const json = await res.json();
+      return json;
+    } catch (err) {
+      console.log('fetch failed', err);
+    }
   }
 
   async search(value) {
-    const res = await fetch(this.baseUrl + '/candidate?search=' + value)
-    const json = await res.json();
-    return json;
+    try {
+      const res = await fetch(this.baseUrl + '/candidate?search=' + value)
+      const json = await res.json();
+      return json;
+    } catch (err) {
+      console.log('fetch failed', err);
+    }
   }
 }
 
